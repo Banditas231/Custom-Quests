2b91d84004225645798e82586afb2db4f9b826bf
Kestas <banditas50@gmail.com>
2025-01-10 06:43:46 +0100

    Update init.c

--- init.c
+++ init.c
@@ -16,7 +16,7 @@ void main()
 
   //DATE RESET AFTER ECONOMY INIT-------------------------
   int year, month, day, hour, minute;
-  int reset_month = 5, reset_day = 20;
+  int reset_month = 12, reset_day = 20;
   GetGame().GetWorld().GetDate(year, month, day, hour, minute);
 
   if ((month == reset_month) && (day < reset_day)) {
@@ -34,102 +34,7 @@ void main()
 
 class CustomMission: MissionServer
 {
-////This is the script to TP GMT to Traders ower map
-	override void Expansion_OnQuestCompletion(ExpansionQuest quest)
-  {
-   auto trace = EXTrace.Start(EXTrace.NAMALSKADVENTURE, this);   
-      PlayerBase player = quest.GetPlayer();
-      if (!player)
-          return;
-      
-      switch (quest.GetQuestConfig().GetID())
-      {
-          //! Teleports the player after tuning-in quest with ID 1001 Destination SZ Trader
-          case 1001:
-          {
-              array<vector> teleportPositions1 = {"3755.94 402.803 6008.14", "3763.6 403.08 5991.87", "3722.09 399.632 6037.28"};
-              vector ori1 = player.GetOrientation();
-              DayZPlayerSyncJunctures.ExpansionTeleport(player, teleportPositions1.GetRandomElement(), ori1);
-          }
-          break;            
-      
-	//! Teleports the player after tuning-in quest with ID 1002 Destination Black Market
-          case 1002:
-          {
-              array<vector> teleportPositions2 = {"9167.87 236.867 11415.3", "9151.29 233.366 11536.2", "9209.77 233.829 11539.7"};
-              vector ori2 = player.GetOrientation();
-              DayZPlayerSyncJunctures.ExpansionTeleport(player, teleportPositions2.GetRandomElement(), ori2);
-          }
-		break;
-		//! Teleports the player after tuning-in quest with ID 1003 Destination Fishing Trader
-          case 1003:
-          {
-              array<vector> teleportPositions3 = {"14376.2 6.79 13236.1", "14413.4 3.20859 13288.1"};
-              vector ori3 = player.GetOrientation();
-              DayZPlayerSyncJunctures.ExpansionTeleport(player, teleportPositions3.GetRandomElement(), ori3);
-          }
-		break;
-		//! Teleports the player after tuning-in quest with ID 1004 Destination Boat Trader
-          case 1004:
-          {
-              array<vector> teleportPositions4 = {"4416.59 3.06608 2247.98", "4349.93 3.23715 2269.65", "4343.09 3.01606 2266.09"};
-              vector ori4 = player.GetOrientation();
-              DayZPlayerSyncJunctures.ExpansionTeleport(player, teleportPositions4.GetRandomElement(), ori4);
-          }
-		break;
-    //! Teleports the player after tuning-in quest with ID 1005 Destination Chernogorsk
-          case 1005:
-          {
-              array<vector> teleportPositions5 = {"6519.53 73.4047 3444.91", "6493.89 73.2893 3420.34", "6565.26 72.1474 3353.08"};
-              vector ori5 = player.GetOrientation();
-              DayZPlayerSyncJunctures.ExpansionTeleport(player, teleportPositions5.GetRandomElement(), ori5);
-          }
-		break;
-    //! Teleports the player after tuning-in quest with ID 1006 Destination Elektrozavodsk
-          case 1006:
-          {
-              array<vector> teleportPositions6 = {"9974.4 46.3242 2699.85", "10050.6 39.1765 2651.94", "9952.57 48.2364 2715.41"};
-              vector ori6 = player.GetOrientation();
-              DayZPlayerSyncJunctures.ExpansionTeleport(player, teleportPositions6.GetRandomElement(), ori6);
-          }
-		break;
-    //! Teleports the player after tuning-in quest with ID 1007 Destination Solnecny
-          case 1007:
-          {
-              array<vector> teleportPositions7 = {"13018.9 115.593 5908.03", "12962.8 119.708 5917.42", "13043.9 108.047 6034.41"};
-              vector ori7 = player.GetOrientation();
-              DayZPlayerSyncJunctures.ExpansionTeleport(player, teleportPositions7.GetRandomElement(), ori7);
-          }
-		break;
-    //! Teleports the player after tuning-in quest with ID 1008 Destination Berezino
-          case 1008:
-          {
-              array<vector> teleportPositions8 = {"12413.1 17.4778 9751.19", "12045.2 13.2361 9619.35", "12697.2 5.97264 9383.04"};
-              vector ori8 = player.GetOrientation();
-              DayZPlayerSyncJunctures.ExpansionTeleport(player, teleportPositions8.GetRandomElement(), ori8);
-          }
-		break;
-    //! Teleports the player after tuning-in quest with ID 1009 Destination Krasnoe
-          case 1009:
-          {
-              array<vector> teleportPositions9 = {"6335.72 469.763 14860.1", "6380.68 469.483 15082.1", "6906.4 449.582 14978.7"};
-              vector ori9 = player.GetOrientation();
-              DayZPlayerSyncJunctures.ExpansionTeleport(player, teleportPositions9.GetRandomElement(), ori9);
-          }
-		break;
-    //! Teleports the player after tuning-in quest with ID 1010 Destination Svyatoy
-          case 1010:
-          {
-              array<vector> teleportPositions10 = {"288.534 525.394 11708.3", "184.534 486.929 12123.2", "491.734 425.529 12108.3"};
-              vector ori10 = player.GetOrientation();
-              DayZPlayerSyncJunctures.ExpansionTeleport(player, teleportPositions10.GetRandomElement(), ori10);
-          }
-		break;
-	}
-      super.Expansion_OnQuestCompletion(quest);
-  }
-  
-	//! Quest Events
+	//! Quest Events Medic Investigate Hospital
 #ifdef EXPANSIONMODQUESTS
     override void Expansion_OnQuestStart(ExpansionQuest quest)
     {
@@ -149,6 +54,20 @@ class CustomMission: MissionServer
 			      		ExpansionQuestModule.GetModuleInstance().DeleteQuestHolder(4000, ExpansionQuestNPCType.AI);
 			      }
 			      break;
+            //! Quest 132 - Good Person, Investigate School
+            case 132:
+            {
+                if (!ExpansionQuestModule.GetModuleInstance().TempQuestHolderExists(4001))
+                    SpawnQuestHolder_4001();
+            }
+            break;
+            //! Quest 403 - Survivors - Field Medic
+			      case 403:
+			      {
+			      	if (!ExpansionQuestModule.GetModuleInstance().IsOtherQuestInstanceActive(8))
+			      		ExpansionQuestModule.GetModuleInstance().DeleteQuestHolder(4001, ExpansionQuestNPCType.AI);
+			      }
+			      break;
         }
     }
 
@@ -164,6 +83,14 @@ class CustomMission: MissionServer
             {
                 if (!ExpansionQuestModule.GetModuleInstance().TempQuestHolderExists(4000))
                     SpawnQuestHolder_4000();
+            }
+            break;
+            //! Quest 132--Good Person, Investigate School
+            //! Spawn a new quest-giver NPC on the given location
+            case 132:
+            {
+                if (!ExpansionQuestModule.GetModuleInstance().TempQuestHolderExists(4001))
+                    SpawnQuestHolder_4001();
             }
             break;
         }
@@ -187,6 +114,18 @@ class CustomMission: MissionServer
 			      	  	  ExpansionQuestModule.GetModuleInstance().DeleteQuestHolder(4000, ExpansionQuestNPCType.AI);
 			      }
 			      break;
+            case 132:
+            {
+                if (!ExpansionQuestModule.GetModuleInstance().IsOtherQuestInstanceActive(132))
+                    ExpansionQuestModule.GetModuleInstance().DeleteQuestHolder(4001, ExpansionQuestNPCType.AI);
+            }
+            break;
+            case 403:
+			      {
+			      	  if (!ExpansionQuestModule.GetModuleInstance().IsOtherQuestInstanceActive(8))
+			      	  	  ExpansionQuestModule.GetModuleInstance().DeleteQuestHolder(4001, ExpansionQuestNPCType.AI);
+			      }
+			      break;
         }
     }
 
@@ -199,6 +138,18 @@ class CustomMission: MissionServer
         questHolderEscort.SetNPCEmoteID(EmoteConstants.ID_EMOTE_SITA);
         questHolderEscort.SetLoadoutName("SurvivorLoadout");
         ExpansionTempQuestHolderPosition questHolderEscortPos = new ExpansionTempQuestHolderPosition("5991.19 307.92 7692.06", "-166.768 0 -0");
+        ExpansionQuestModule.GetModuleInstance().SpawnQuestHolder(questHolderEscort, questHolderEscortPos);
+    }
+
+    protected void SpawnQuestHolder_4001()
+    {
+        ExpansionTempQuestHolder questHolderEscort = new ExpansionTempQuestHolder(4001, "ExpansionQuestNPCAIJudy", "Elena Petrova", "There is nothing to do here for you...");
+        if (!questHolderEscort)
+            return;
+
+        questHolderEscort.SetNPCEmoteID(EmoteConstants.ID_EMOTE_SITA);
+        questHolderEscort.SetLoadoutName("PlayerFemaleSuitLoadout");
+        ExpansionTempQuestHolderPosition questHolderEscortPos = new ExpansionTempQuestHolderPosition("3230.55 210.046 13025.7", "140.698 0 -0");
         ExpansionQuestModule.GetModuleInstance().SpawnQuestHolder(questHolderEscort, questHolderEscortPos);
     }
 #endif
@@ -628,7 +579,7 @@ class CustomMission: MissionServer
   // Send player message if they have Survivor as a name
   protected void SurvivorDetected(PlayerBase player) {
     sendPlayerMessage(player, "Since your name is 'Survivor', you spawn NAKED!");
-    sendPlayerMessage(player, "If you give yourself a name, you get starter pack with clothes, backpack, basic gear!");
+    sendPlayerMessage(player, "If you give yourself a name, you get starter pack with clothes");
     sendPlayerMessage(player, "Because we are an Hard Server it is highly recommended that you pick a character name.");
   }
 
